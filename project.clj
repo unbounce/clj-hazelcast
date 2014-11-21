74ac8b04df035784f0c191ab7fcfaf334afc7917
David Dossot <david@dossot.net>
2014-11-21 14:56:38 -0800

    changed target version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.unbounce/clj-hazelcast "1.0.3.UB"
+(defproject com.unbounce/clj-hazelcast "1.0.2.RC2"
   :description "Clojure library for the Hazelcast p2p cluster"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.runa/clj-kryo "1.4.1"]
