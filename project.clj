496a91acd483b80f59d18f415c67521e97dc6a40
David Dossot <david@dossot.net>
2015-02-26 18:08:30 -0800

    reverted to runa's clj-kryo

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.unbounce/clj-hazelcast "1.2.1"
   :description "Clojure library for the Hazelcast p2p cluster"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.unbounce/clj-kryo "1.5.0"]
+                 [org.clojars.runa/clj-kryo "1.5.0"]
                  [com.hazelcast/hazelcast "3.4"]]
   :profiles {:provided {:dependencies [[org.clojure/tools.logging "0.2.6"]]}}
   :global-vars {*warn-on-reflection* true}
